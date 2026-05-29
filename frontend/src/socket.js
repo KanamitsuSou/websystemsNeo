d09bc0866a2d4954af1f76e36953773605220fb6
金光奏 <souzaemon011965@gmail.com>
2026-05-29 23:27:41 +0900

    change

--- frontend/src/socket.js
+++ frontend/src/socket.js
@@ -2,4 +2,4 @@ import { io } from 'socket.io-client';
 
 
 const BACKEND_URL = import.meta.env.VITE_BACKEND_URL || 'https://websystem1890908989-3.onrender.com';
-export const socket = io(BACKEND_URL,{transport : ["websocket"]});
+export const socket = io(BACKEND_URL);
