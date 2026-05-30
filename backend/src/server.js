7f55d2fd5295a8d761420391c688edb1c7ecaa70
金光奏 <souzaemon011965@gmail.com>
2026-05-30 10:14:11 +0900

    socket configuration

--- backend/src/server.js
+++ backend/src/server.js
@@ -14,7 +14,8 @@ const io = new Server(server, {
   cors: {
     origin: "*", 
     methods: ["GET", "POST"]
-  }
+  },
+  transports: ['websocket']
 });
 // ーーーーーーーーーーーーーーーーーーーーーーーーーーーー
 
--- frontend/src/socket.js
+++ frontend/src/socket.js
@@ -2,4 +2,6 @@ import { io } from 'socket.io-client';
 
 
 const BACKEND_URL = import.meta.env.VITE_BACKEND_URL || 'https://websystem1890908989-3.onrender.com';
-export const socket = io(BACKEND_URL);
+export const socket = io(BACKEND_URL,{
+  transports: ['websocket'] // 👈 フロント側にもこれを追加！（Pollingの挨拶をスキップさせる）
+});
